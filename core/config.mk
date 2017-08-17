0a30502017d27f54b58cea2d310f4bebb34e9a0d
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Syberia build support (take 2)
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include Lineage pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use lineage pathmap
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
    Change-Id: I2bb37f2e44efaf31a721003eded075fa2191ebaf
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1244,9 +1244,6 @@ endif
 ## last, to avoid accidental resetting by device configs
 # $(eval include vendor/reloaded/sepolicy/common/sepolicy.mk)
 
-# Include any vendor specific config.mk file
--include $(TOPDIR)vendor/*/build/core/config.mk
-
 ifneq ($(SYBERIA_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
