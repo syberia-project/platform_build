ff0a87d02f1cb481d8d1a75e597470294a97041d
Simon Shields <simon@lineageos.org>
2017-08-23 12:06:18 +1000

    soong: include syberia board flags
    Change-Id: I1aabe4915036790dd35ceb985cf0a2701ca0faf9

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -40,6 +40,8 @@ invert_bool = $(if $(strip $(1)),,true)
 $(shell mkdir -p $(dir $(SOONG_VARIABLES)))
 _contents := {$(newline)
 
+include vendor/syberia/build/soong/soong_config.mk
+
 $(call add_json_str,  Make_suffix, -$(TARGET_PRODUCT))
 
 $(call add_json_str,  BuildId,                           $(BUILD_ID))
