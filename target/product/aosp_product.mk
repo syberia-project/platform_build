c6a3c63c630fd301d6074812afc05223ea6a8423
Danny Lin <danny@kdrag0n.dev>
2021-05-05 00:29:27 -0700

    product: Remove default AOSP sounds
    We'll ship a different collection of sounds in our vendor module.
    
    Change-Id: I30383b8fd75c1171ce3f4c5059d75995c0399bad

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -18,13 +18,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-# Default AOSP sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.com.android.dataroaming=true \
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
